c9f4085e5678cd9f01dc15bf29c4f14bb9192351
patte <patrick.recher@gmail.com>
2015-11-11 14:22:16 +0100

    honour kubernetes env vars to build MONGO_URL
    get the kubernetes mongo service name from K8S_MONGO_SERVICE_ENV_NAME
    and expand it to the resulting MONGO_URL
    if K8S_MONGO_DB_NAME is set, it gets appended to MONGO_URL
    ex.
    given by k8s env:
    MYAPP_MONGO_SERVICE_HOST="host"
    MYAPP_MONGO_SERVICE_PORT=27018
    set in Dockerfile/controller.json
    K8S_MONGO_SERVICE_ENV_NAME="MYAPP_MONGO"
    K8S_MONGO_DB_NAME="myapp_prod"
    results in:
    MONGO_URL: mongodb://host:27018:/myapp_prod

--- base/scripts/run_app.sh
+++ base/scripts/run_app.sh
@@ -40,5 +40,30 @@ fi
 # Honour already existing PORT setup
 export PORT=${PORT:-80}
 
+# map kubernetes service env vars
+# get the kubernetes mongo service name from K8S_MONGO_SERVICE_ENV_NAME
+# and expand it to the resulting MONGO_URL
+# if K8S_MONGO_DB_NAME is set, it gets appended to MONGO_URL
+# ex.
+# given by k8s env:
+# MYAPP_MONGO_SERVICE_HOST="host"
+# MYAPP_MONGO_SERVICE_PORT=27018
+# set in Dockerfile/controller.json
+# K8S_MONGO_SERVICE_ENV_NAME="MYAPP_MONGO"
+# K8S_MONGO_DB_NAME="myapp_prod"
+# results in:
+# MONGO_URL: mongodb://host:27018:/myapp_prod
+if [[ $K8S_MONGO_SERVICE_ENV_NAME ]]; then
+  MONGO_HOST_VAR=${K8S_MONGO_SERVICE_ENV_NAME}_SERVICE_HOST
+  MONGO_PORT_VAR=${K8S_MONGO_SERVICE_ENV_NAME}_SERVICE_PORT
+
+  MONGO_URL=mongodb://${!MONGO_HOST_VAR}:${!MONGO_PORT_VAR}
+  if [[ $K8S_MONGO_DB_NAME ]]; then
+    MONGO_URL=${MONGO_URL}:/${K8S_MONGO_DB_NAME}
+  fi
+  export MONGO_URL=${MONGO_URL}
+  echo "Use MONGO_URL: $MONGO_URL"
+fi
+
 echo "=> Starting meteor app on port:$PORT"
 node main.js
